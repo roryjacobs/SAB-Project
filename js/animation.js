0a59cb642cdcb94b101a2ceab7b186fdd7c15a99
Dustin J SIlk <dustinsilk@yahoo.com>
2013-02-27 16:39:12 +0200

    Animates according to page width

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,13 @@
-var $positionOne = 500;
+var newPosition = 50;
+var barOne = document.getElementById("barOne");
+var barOneWidth = barOne.clientWidth;
+
+var $movePositionOne = newPosition / 100 * barOneWidth;
 var $positionTwo;
-        	
+     	
 $(document).ready(function() {				
 	$('#capOne').animate({
-       	marginLeft: $positionOne,
+       	marginLeft: $movePositionOne,
 	 }, 1000, function() {
 				});								  
 });
