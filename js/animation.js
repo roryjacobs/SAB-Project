0428325f3cfcd266529c4e35958cfafa70825aa3
Dustin J SIlk <dustinsilk@yahoo.com>
2013-02-27 16:56:57 +0200

    Update

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,35 @@
-var newPosition = 50;
-var barOne = document.getElementById("barOne");
-var barOneWidth = barOne.clientWidth;
+//Total to be sold to win
+var totalAmount = 1000;
 
-var $movePositionOne = newPosition / 100 * barOneWidth;
-var $positionTwo;
+//New positions to be recieved by Server
+var newPositionOne = 300;
+var newPositionTwo = 100;
+
+//set bar names
+var barOne = document.getElementById("barOne");
+var barTwo = document.getElementById("barTwo");
+
+//calcuation for movements
+//Bar one
+var barOneWidth = barOne.clientWidth;
+var $movePositionOne = newPositionOne / totalAmount * barOneWidth;
+//bar two
+var barTwoWidth = barTwo.clientWidth;
+var $movePositionTwo = newPositionTwo / totalAmount * barTwoWidth;
      	
-$(document).ready(function() {				
-	$('#capOne').animate({
-       	marginLeft: $movePositionOne,
-	 }, 1000, function() {
-				});								  
+//Run animation at start of script.
+$(document).ready(function() {
+	animation();
 });
+
+//Main Animation Function
+function animation(){
+	$('#capOne').animate({marginLeft: $movePositionOne,}, totalAmount, function(){});
+	$('#capTwo').animate({marginLeft: $movePositionTwo,}, totalAmount, function(){});
+
+}
+
+
+
+
 
